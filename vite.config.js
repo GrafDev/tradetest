9deea0307eafeb75d83db57006c0021ba94bfe89
GrafDev <gregory.yakovlev@gmail.com>
2024-11-11 23:07:25 +0100

    fix UX

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        define: {
+            'process.env': env
+        },
+        plugins: [react()],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+                '@components': path.resolve(__dirname, './src/components'),
+                '@lib': path.resolve(__dirname, './src/lib'),
+                '@styles': path.resolve(__dirname, './src/styles'),
+                '@utils': path.resolve(__dirname, './src/utils'),
+                '@hooks': path.resolve(__dirname, './src/hooks'),
+                '@providers': path.resolve(__dirname, './src/providers')
+            }
+        }
+    };
+});
